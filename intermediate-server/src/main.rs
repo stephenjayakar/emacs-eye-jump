d4dbb59e463a24f4890db9dbadcf85a1072c97b4
Stephen Jayakar <sjayakar@plaid.com>
2021-10-08 12:26:52 -0700

    meow

--- intermediate-server/src/main.rs
+++ intermediate-server/src/main.rs
@@ -1,3 +1,5 @@
+#[macro_use] extern crate rocket;
+
 use enigo::*;
 use std::{thread, time};
 
@@ -5,20 +7,37 @@ use std::{thread, time};
 const SCREEN_WIDTH: i32 = 2560;
 const SCREEN_HEIGHT: i32 = 1440;
 
+#[get("/")]
+fn hello() -> &'static str {
+    "Hello, world!"
+}
+
+#[post("/jump")]
+fn hello() -> &'static str {
+    "Hello, world!"
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![hello])
+}
+
 // -> (x_screen, y_screen)
 fn screen_coords_from_ratio(x: f32, y: f32) -> (i32, i32) {
     ((SCREEN_WIDTH as f32 * x) as i32, (SCREEN_HEIGHT as f32 * y) as i32)
 }
 
-fn main() {
-    let mut enigo = Enigo::new();
-    let coords = screen_coords_from_ratio(0.5, 0.5);
-    println!("{:?}", coords);
-    enigo.mouse_move_to(coords.0, coords.1);
-    let sec = time::Duration::from_secs(1);
-    thread::sleep(sec);
-    enigo.mouse_down(MouseButton::Left);
-    // enigo.mouse_move_relative(100, 100);
-    enigo.mouse_up(MouseButton::Left);
+// fn main() {
+//     let mut enigo = Enigo::new();
+//     let coords = screen_coords_from_ratio(0.5, 0.5);
+//     println!("{:?}", coords);
+//     enigo.mouse_move_to(coords.0, coords.1);
+//     let sec = time::Duration::from_secs(1);
+//     thread::sleep(sec);
+//     enigo.mouse_down(MouseButton::Left);
+//     enigo.mouse_up(MouseButton::Left);
+// }
+
     // enigo.key_sequence("hello world");
-}
+    // enigo.mouse_move_relative(100, 100);
+
